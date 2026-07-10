92331b73d17aaefd0e4c198ac576075ec2393f90
Raghav0079 <raghav051006@gmail.com>
2026-07-10 10:40:20 +0530

    commit 5

--- Variables.scala
+++ Variables.scala
@@ -3,14 +3,14 @@ object Basics {
     println("--- 1. Variables & Types ---")
     
     // val: Immutable 
-    val message: String = "Hello, Data Scientist!" 
+    //val message: String = "Hello, Data Scientist!" 
     // message = "New String" // This would cause a compile error
     
     // var: Mutable (Changeable)
     var counter: Int = 0
     counter = counter + 1
     
-    println(s"Message: $message") // String interpolation with 's'
+    //println(s"Message: $message") // String interpolation with 's'
     println(s"Counter: $counter")
     
     val pi = 3.14159 
