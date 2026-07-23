d0a0c8e4b1ebdaf5bc7f7f39ea0dca9eb43edf35
Raghav0079 <raghav051006@gmail.com>
2026-07-23 12:30:33 +0530

    commit 11

--- Loops.scala
+++ Loops.scala
@@ -2,18 +2,21 @@ object Loops {
   def main(args: Array[String]): Unit = {
     println("--- 3. Loops ---")
     
-    // Example of a 'for' loop
+    // Example of a 'for' loop  
     println("For loop from 1 to 5:")
-    for (i <- 1 to 5) {
+    for (i <- 1 to 5) {   // entry loop 
       println(i)
     }
     
-    // Example of a 'while' loop
+    // Example of a 'while' loop  
     println("While loop counting down from 5:")
     var count = 5
-    while (count > 0) {
+    while (count > 0) { // entry loop
       println(count)
       count -= 1
     }
+
+    // do while is exit loop
+    
   }
 }
