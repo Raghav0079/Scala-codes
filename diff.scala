dd9d4254c4c2d202c1fe179f62df8cf84290ef5c
Raghav0079 <raghav051006@gmail.com>
2026-07-27 11:23:57 +0530

    commit 19

---
+++ diff.scala
@@ -0,0 +1,12 @@
+object difference {
+    def main(args: Array[String]): Unit = {
+        val x = 40
+        var y = 50
+
+        println(x)
+        println(y) 
+
+        y = 65 
+        println(y)
+    }
+}
