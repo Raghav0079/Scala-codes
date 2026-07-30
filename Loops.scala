9380722e63605343936f31947aa5686f67467580
Raghav0079 <raghav051006@gmail.com>
2026-07-30 13:37:59 +0530

    commit 27

--- Loops.scala
+++ Loops.scala
@@ -6,17 +6,19 @@ object Loops {
       println(i)
     }
     
-    
     println("While loop counting up from 1 to 15:")
     var count = 1
-    while (count < 15) { 
+    while (count <= 15) { 
       println(count)
       count += 1
     }
 
-
-
-  
-    
+    var marks = 35
+    if(marks > 33){
+      println("pass and promoted to next class")
+    }
+    else{
+      println("fail need to repeat class")
+    }
   }
 }
