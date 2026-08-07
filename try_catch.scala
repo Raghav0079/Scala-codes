61cff22540e6cd9db1da269824b857b4426a0464
Raghav0079 <raghav051006@gmail.com>
2026-08-07 10:42:43 +0530

    commit 34

--- try_catch.scala
+++ try_catch.scala
@@ -1,13 +1,13 @@
 object HelloWorld {
   def main(args: Array[String]): Unit = {
-// val result: Int =
-//   try {
-//     10 / 0
-//   } catch {
-//     case _: ArithmeticException => -1
-//   } finally {
-//     println("cleanup runs regardless")   // finally's return value is discarded, for side effects only
-//   }
+val result: Int =
+    try {
+    10 / 0
+    } catch {
+     case _: ArithmeticException => -1
+    } finally {
+    println("cleanup runs regardless")   // finally's return value is discarded, for side effects only
+    }
 
 val s = 10/0
 print(s)
