64160e83bd7a65f33dd38733cc80fd02dd8fd817
Raghav0079 <raghav051006@gmail.com>
2026-09-20 09:05:13 +0530

    commit 65

--- p2.scala
+++ p2.scala
@@ -1,6 +1,25 @@
-/*Given a string "Scala is fun", write functions to: reverse it, count vowels, and check if it's a palindrome
-(test with "madam")*/
+object Main {
 
-object Main{
-    
+  def main(args: Array[String]): Unit = {
+
+    val string = "Scala is fun"
+    val test = "madam"
+    //  Reverse 
+    def reverseString(str: String): String = str.reverse
+
+    // Count vowels
+    def countVowels(str: String): Int = 
+      str.count(char => "aeiouAEIOU".contains(char))
+
+    // Check for palindrome
+    def isPalindrome(str: String): Boolean = 
+      str.toLowerCase == str.toLowerCase.reverse
+
+    // Output
+    println(s"Reversed: '${reverseString(string)}'")
+    println(s"Vowel count: ${countVowels(string)}")
+    println(s"Is '$string' a palindrome? ${isPalindrome(string)}")
+    println(s"Is '$test' a palindrome? ${isPalindrome(test)}")
+  }
+
 }
